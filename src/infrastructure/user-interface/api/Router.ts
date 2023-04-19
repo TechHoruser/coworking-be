4f75f6e497614ed4f7ed810b6c2e87dac80d09e5
Francisco Javier Ponce Rosales <franciscojavier.poncerosales@gmail.com>
2023-04-19 23:40:38 +0200

    feat: :sparkles: add middleware in routers with new code

--- src/infrastructure/user-interface/api/Router.ts
+++ src/infrastructure/user-interface/api/Router.ts
@@ -1,10 +1,60 @@
-import express, { Router } from 'express';
+import express, { RequestHandler, Router } from 'express';
 import AuthRouter from '@/infrastructure/user-interface/api/auth/Router';
 import MessageRouter from '@/infrastructure/user-interface/api/message/Router';
+import RoomRouter from '@/infrastructure/user-interface/api/room/Router';
+import { JwtDecodeMiddleware } from '@/infrastructure/auth/JwtDecodeMiddleware';
 
 const router: Router = express.Router();
 
-router.use('/auth', AuthRouter);
-router.use('/message', MessageRouter);
+interface routerInterface {
+  path: string,
+  router: Router,
+}
+
+interface MiddlewareInterface {
+  middleware: RequestHandler,
+  routers: Router[],
+}
+
+const routes: routerInterface[] = [
+  {
+    path: '/auth',
+    router: AuthRouter,
+  },
+  {
+    path: '/message',
+    router: MessageRouter,
+  },
+  {
+    path: '/room',
+    router: RoomRouter,
+  },
+];
+
+const middlewareUses: MiddlewareInterface[] = [
+  {
+    middleware: JwtDecodeMiddleware,
+    routers: [
+      MessageRouter,
+      RoomRouter,
+    ],
+  },
+];
+
+routes.forEach((route: routerInterface) => {
+  const middlewares: RequestHandler[] = [];
+
+  middlewareUses.forEach((middlewareUse) => {
+    if (middlewareUse.routers.includes(route.router)) {
+      middlewares.push(middlewareUse.middleware);
+    }
+  });
+
+  router.use(
+    route.path,
+    ...middlewares,
+    route.router,
+  );
+});
 
 export default router;
