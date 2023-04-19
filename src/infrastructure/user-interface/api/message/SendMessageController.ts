552915b7047875ec818f20baa68ef5317e30216b
Francisco Javier Ponce Rosales <franciscojavier.poncerosales@gmail.com>
2023-04-20 00:05:10 +0200

    feat: :passport_control: add passport for get user user id when send message

--- src/infrastructure/user-interface/api/message/SendMessageController.ts
+++ src/infrastructure/user-interface/api/message/SendMessageController.ts
@@ -1,23 +1,29 @@
 import { Request, Response } from 'express';
+import * as passport from 'passport';
 import { injectable } from 'inversify';
 import { prisma } from '@/infrastructure/persistence/PrismaClient';
 
 @injectable()
 export class SendMessageController {
-  async invoke(req: Request, res: Response): Promise<void> {
+  async invoke(req: Request, res: Response) {
     const {
       content,
       roomId,
-    } = req.body;
+    } = req.body as {
+      content: string,
+      roomId: string,
+    };
+
+    const userId = req.user as string;
 
     const message = await prisma.message.create({
       data: {
         content,
-        userId: '',
+        userId,
         roomId,
       },
     });
 
-    res.send(message);
+    res.status(201).send(message);
   }
 }
