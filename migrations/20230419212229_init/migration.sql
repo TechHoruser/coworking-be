d5f4ea993d5529c306f6d810d3f94cb23cd129dd
Francisco Javier Ponce Rosales <franciscojavier.poncerosales@gmail.com>
2023-04-19 23:26:57 +0200

    feat: :card_file_box: update database

--- migrations/20230419212229_init/migration.sql
+++ migrations/20230419212229_init/migration.sql
@@ -20,8 +20,8 @@ CREATE TABLE "Message" (
     "id" TEXT NOT NULL PRIMARY KEY,
     "content" TEXT NOT NULL,
     "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "userId" TEXT NOT NULL,
+    "userId" TEXT,
     "roomId" TEXT NOT NULL,
-    CONSTRAINT "Message_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User" ("id") ON DELETE RESTRICT ON UPDATE CASCADE,
+    CONSTRAINT "Message_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User" ("id") ON DELETE SET NULL ON UPDATE CASCADE,
     CONSTRAINT "Message_roomId_fkey" FOREIGN KEY ("roomId") REFERENCES "Room" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
 );
